7f8ea61c042d3a122535403383077d03dbfc6642
Advait Dhakad <advaitdhakad@gmail.com>
2025-04-06 19:10:33 +0530

    testing w/o navbar

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="w-full">
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <Grid />
         <Certificates />
