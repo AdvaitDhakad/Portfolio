adcd53dc81d53da34c3b729ebb3c7e0993a61e82
Advait Dhakad <59308251+AdvaitDhakad@users.noreply.github.com>
2025-04-07 13:09:38 +0530

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // enables app directory (usually default in latest Next.js)
+  },
+};
+
+module.exports = nextConfig;
